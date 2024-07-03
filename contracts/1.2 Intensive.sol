4df0d18251c2ec8f890732a4d81457cfe1fc71d0
yavityai <yavityai@mail.ru>
2024-07-03 17:37:49 +0700

    second contract

---
+++ contracts/1.2 Intensive.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract Demo {
+
+    bool public firstBool = true;
+    bool private myBool;
+
+    uint public numberOne = 123;
+    int private numberTwo = 234;
+    int public numberThree = 345;
+
+}
